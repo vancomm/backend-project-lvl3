d5e5bf549f9f7c92e0e25cbf65bc2aa113981978
vancomm <vanushnick@gmail.com>
2021-08-19 16:38:03 +0300

    Implemented pageloader function; it can now load html from a URL

--- __tests___/pageloader.test.js
+++ __tests___/pageloader.test.js
@@ -1,6 +1,42 @@
+import os from 'os';
+import fs from 'fs/promises';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+import nock from 'nock';
 import pageLoader from '../index.js';
 
-test('stuff', () => {
-  pageLoader();
-  expect(null).toBeNull();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
+const fixturePath = getFixturePath('sample.html');
+
+const url = new URL('https://sample.com/path/to/site.html');
+
+const dirprefix = path.join(os.tmpdir(), 'page-loader-');
+const filename = 'sample-com-path-to-site.html';
+
+let expected;
+let dirpath;
+let filepath;
+
+beforeEach(async () => {
+  expected = await fs.readFile(fixturePath, 'utf-8');
+
+  dirpath = await fs.mkdtemp(dirprefix);
+  filepath = path.join(dirpath, filename);
+});
+
+test('stuff', async () => {
+  const scope = nock(url.origin)
+    .get(url.pathname)
+    .reply(200, expected);
+
+  const result = await pageLoader(url, dirpath);
+  const actual = await fs.readFile(result, 'utf-8');
+
+  expect(scope.isDone()).toBe(true);
+  expect(result).toEqual(filepath);
+  expect(actual).toEqual(expected);
 });
--- src/pageloader.js
+++ src/pageloader.js
@@ -1,5 +1,22 @@
-const pageLoader = () => {
+import path from 'path';
+import fs from 'fs/promises';
+import axios from 'axios';
 
+const makeFileName = (url) => {
+  const hostname = url.hostname.replace(/[^\da-zA-Z]/g, '-');
+  const [pathname] = url.pathname.split('.').map((s) => s.replace(/[^\da-zA-Z]/g, '-'));
+  return `${hostname + pathname}.html`;
+};
+
+const pageLoader = (url, dirpath) => {
+  const filename = makeFileName(url);
+  const filepath = path.join(dirpath, filename);
+  return axios.get(url.toString())
+    .then((response) => fs.writeFile(filepath, response.data))
+    .then(() => filepath)
+    .catch((error) => {
+      throw error;
+    });
 };
 
 export default pageLoader;
